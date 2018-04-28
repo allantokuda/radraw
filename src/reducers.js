41e963d595b927c7c14b969d7d6cdc12987c0df3
Allan Tokuda <praetis@gmail.com>
2018-04-28 18:23:25 -0500

    Update arrows to latest render of operator shape

--- src/reducers.js
+++ src/reducers.js
@@ -10,49 +10,57 @@ import { connectionPoints } from './operatorShape'
 // temporary
 import initialState from './initial-state'
 
+const updatedNode = (node, action) => {
+  let changes
+
+  switch (action.type) {
+    case MOVE_NODE:
+      changes = { x: node.x + action.dx, y: node.y + action.dy }
+      break
+
+    case RENAME_NODE:
+      changes = { relation: { name: action.name } }
+      break
+
+    case RESIZE_NODE:
+      let hasOperator = Object.keys(node.operator).length > 0
+      changes = {
+        height: action.nodeHeight,
+        operator: hasOperator ? Object.assign({}, node.operator, {
+          width: action.operatorWidth,
+          height: action.operatorHeight
+        }) : {}
+      }
+      break
+
+    case UPDATE_OPERATOR_PARAM:
+      let val = {}
+      val[action.paramName] = action.value
+      changes = {
+        operator: Object.assign({}, node.operator, {
+          params: Object.assign({}, node.operator.params, val)
+        })
+      }
+      break
+
+    default:
+      changes = {}
+  }
+
+  return Object.assign({}, node, changes)
+}
+
 const nodes = (state, action) => state.nodes.map(node => {
   if (node.id === action.nodeId) {
-    let changes
-    switch (action.type) {
-      case MOVE_NODE:
-        changes = { x: node.x + action.dx, y: node.y + action.dy }
-        break
-
-      case RENAME_NODE:
-        changes = { relation: { name: action.name } }
-        break
-
-      case RESIZE_NODE:
-        let hasOperator = Object.keys(node.operator).length > 0
-        changes = {
-          height: action.nodeHeight,
-          operator: hasOperator ? Object.assign({}, node.operator, {
-            width: action.operatorWidth,
-            height: action.operatorHeight
-          }) : {}
-        }
-        break
-
-      case UPDATE_OPERATOR_PARAM:
-        let val = {}
-        val[action.paramName] = action.value
-        changes = {
-          operator: Object.assign({}, node.operator, {
-            params: Object.assign({}, node.operator.params, val)
-          })
-        }
-        break
-
-      default:
-        changes = {}
-    }
-    return Object.assign({}, node, changes)
+    return updatedNode(node, action)
   }
   return node
 })
 
 const arrows = (state, action) => {
-  const changedNode = state.nodes.find(node => node.id === action.nodeId)
+  const oldNode = state.nodes.find(node => node.id === action.nodeId)
+  const changedNode = updatedNode(oldNode, action)
+
   return state.arrows.map(arrow => {
     if (arrow.from === action.nodeId) {
       return Object.assign({}, arrow, { x1: changedNode.x, y1: changedNode.y + (changedNode.height || 0) })
@@ -66,7 +74,7 @@ const arrows = (state, action) => {
 }
 
 export default function reducer(state = initialState, action) {
-  //console.log(state)
+  //console.log(state, action)
   return {
     nodes: nodes(state, action),
     arrows: arrows(state, action)
