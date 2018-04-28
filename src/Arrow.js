a9f6d0128307362e746d864470421b1751cfbcd9
Allan Tokuda <praetis@gmail.com>
2018-04-28 17:19:35 -0500

    Reorient arrow downward WIP

--- src/Arrow.js
+++ src/Arrow.js
@@ -3,22 +3,29 @@ import React from 'react'
 const Arrow = ({ x1, y1, x2, y2 }) => {
 
   const length = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-  const arrowDx = 10
-  const arrowDy = 4
+  const headLength = 10
+  const headWidth = 8
 
-  const arrowPoints = [[length - arrowDx, 0], [length, arrowDy], [length - arrowDx, arrowDy * 2]].map(point => point.join(',')).join(' ')
+  // Draw arrow pointing downward as baseline because this app typically draws downward arrows
+  const arrowPoints = [
+    [-headWidth / 2, length - headLength], // left edge of arrow head's base
+    [             0, length             ], // tip of arrow head
+    [ headWidth / 2, length - headLength]  // right edge of arrow head's base
+  ].map(point => point.join(',')).join(' ')
 
-  const svgStyle = {
-    width: length,
-    height: arrowDy * 2,
-    left: (x2 + x1) / 2 - length / 2,
-    top: (y2 + y1) / 2 - arrowDy,
-    transform: 'rotate(' + Math.atan2(y2 - y1, x2 - x1) + 'rad)'
+  const svgPosition = {
+    width: headWidth,
+    height: length,
+    left: (x2 + x1) / 2 - headWidth,
+    top: (y2 + y1) / 2 - length / 2,
+    transform: 'rotate(' + Math.atan2(x2 - x1, y2 - y1) + 'rad)'
   }
    
-  return <svg className="arrow" style={svgStyle}>
-    <line className="arrowLine" x1="0" x2={length-arrowDx} y1={arrowDy} y2={arrowDy}/>
-    <polygon className="arrowHead" points={arrowPoints} />
+  return <svg className="arrow" style={svgPosition}>
+    <g transform={"translate(" + headWidth / 2 + ", 0)"}>
+      <line className="arrowLine" x1="0" x2="0" y1="0" y2={length-headLength}/>
+      <polygon className="arrowHead" points={arrowPoints} />
+    </g>
   </svg>
 }
 
