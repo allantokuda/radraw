61e16f76aa829266c182f0ca5d579dd144282546
Allan Tokuda <praetis@gmail.com>
2020-10-11 07:22:10 -0500

    Class for editor state for later features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import classNames from 'classnames'
 import Toolbar from './Toolbar'
 import PrecedenceChart from './PrecedenceChart'
 import './App.css'
 import './z-index.css'
 
-let App = ({ state }) => (
-  <div className="App">
-    <Toolbar />
-    <PrecedenceChart />
-  </div>
-)
+let App = ({ state }) => {
+  let classNamesObj = { App: true }
+  classNamesObj[state.editor.action] = true
+
+  return (
+    <div className={classNames(classNamesObj)}>
+      <Toolbar />
+      <PrecedenceChart />
+    </div>
+  )
+}
 
 App = connect(state => ({state}))(App);
 
