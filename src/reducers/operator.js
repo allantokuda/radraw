df4905ef455339d4d6f8a1c1e1b3475d83ca78fd
Allan Tokuda <praetis@gmail.com>
2020-11-20 09:27:03 -0600

    NSE match join with dashed border

--- src/reducers/operator.js
+++ src/reducers/operator.js
@@ -3,28 +3,38 @@ export default (operator, action) => {
     case 'UPDATE_OPERATOR_PARAMS':
       let shape = operator.shape
       let type = operator.type
-
-      const search = action.value.toLowerCase().replace(' ', '')
+      let style = {}
 
       if (operator.type.match(/Match Join/)) {
         let orientation = shape.match('Left') ? 'Left' : 'Right'
 
-        if (search.match(/aid\([em]\)/) && search.match(/bid\([sod]\)/)) {
+        const search = action.value.toLowerCase().replace(' ', '')
+        const aid = (search.match(/aid\((.)\)/) || [])[1] || ''
+        const bid = (search.match(/bid\((.)\)/) || [])[1] || ''
+
+        if (aid === 'n' || bid === 'n' || aid.match(/[em]/) && bid.match(/[sod]/)) {
           type = 'Bad Match Join'
+          style = { stroke: 'red', fill: '#fee' }
         } else {
           type = 'Match Join'
         }
 
-        if (search.match(/aid\([sod]\)/) && search.match(/bid\([sod]\)/)) {
+        if (aid.match(/[sod]/) && bid.match(/[sod]/)) {
           shape = 'FullHouse' + orientation
-        } else if (search.match(/aid\([em]\)/) && search.match(/bid\([em]\)/)) {
+
+        } else if (aid === bid && aid.match(/[em]/) && bid.match(/[em]/)) {
           shape = 'HalfHouse' + orientation + 'Symmetric'
-        } else if (search.match(/bid\([em]\)/)) {
+
+        } else if (aid !== bid && aid.match(/[em]/) && bid.match(/[em]/)) {
+          shape = 'HalfHouse' + orientation
+          style = { strokeDasharray: '5 5' }
+
+        } else if (bid.match(/[em]/)) {
           shape = 'HalfHouse' + orientation
         }
       }
 
-      return Object.assign({}, operator, { params: action.value, shape, type })
+      return Object.assign({}, operator, { params: action.value, shape, type, style })
 
     default:
       return operator;
